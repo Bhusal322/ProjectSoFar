544992ee5919da22b579fedf2b6c8e2034a1b7aa
Vishal Kumar <20134730@student.westernsydney.edu.au>
2024-04-25 17:34:50 +1000

    Initial commit

---
+++ resources/views/buyer.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/allstyles.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>Horticulture Website Buyer!!!</h1>
+  <p>Make the world green</p>
+</div>
+
+<div class="navbar">
+    <a href="/html/home.html" class="nav-link">Home</a>
+    <a href="#" class="nav-link">My Projects</a>
+    <a href="#" class="nav-link">Search Sellers</a>
+   <a href="#" class="nav-link">Post Project</a>
+    <a href="/html/myaccount.html" class="nav-link">My Account</a>
+  <a href="/html/login.html" class="right">Log-in</a>
+</div>
+
+
+    <div class="main">
+      
+        <section class="project-posting">
+            <h2>Project Posting</h2>
+            <form>
+                <label for="project-title">Title:</label>
+                <input type="text" id="project-title" name="project-title"><br>
+                <label for="project-description">Description:</label>
+                <textarea id="project-description" name="project-description"></textarea><br>
+                <label for="project-photos">Photos:</label>
+                <input type="file" id="project-photos" name="project-photos" multiple><br>
+                <label for="project-timelines">Timelines:</label>
+                <input type="text" id="project-timelines" name="project-timelines"><br>
+                <input type="submit" value="Post Project">
+            </form>
+        </section>
+
+        <section class="search-filter">
+            <h2>Search and Filter</h2>
+            <!-- Advanced search form -->
+            <form>
+                <!-- Add search fields and filters here -->
+            </form>
+        </section>
+
+        <section class="rfp">
+            <h2>Request for Proposal (RFP)</h2>
+            <!-- Form to send RFPs -->
+            <form>
+                <!-- Add fields to solicit bids here -->
+            </form>
+        </section>
+
+        <section class="ratings-reviews">
+            <h2>Ratings and Reviews</h2>
+            <!-- Display ratings and reviews for sellers -->
+        </section>
+   
+    
+  </div>
+  
+  <footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <h5>Company</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Press Releases</a></li>
+                    <li><a href="#">Partnerships</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <h5>Legal</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">Terms of Service</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Cookie Policy</a></li>
+                    <li><a href="#">Copyright Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <h5>Support</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">FAQs</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Feedback</a></li>
+                    <li><a href="#">Help Center</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <h5>Follow Us</h5>
+                <ul class="list-unstyled list-inline">
+                    <li class="list-inline-item"><a href="#" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+  
+  </body>
+  </html>
+  
---
+++ resources/views/myaccount.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/allstyles.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>Horticulture Website</h1>
+  <p>Make the world green</p>
+</div>
+
+<div class="navbar">
+    
+    <a href="home.html">Home</a>
+    <a href="#">Browse</a>
+   <a href="#">Sell</a>
+    <a href="/html/myaccount.html">My Account</a>
+  <a href="login.html" class="right">Log-in</a>
+</div>
+
+
+<div class="main">
+    <h2>My Account</h2>
+    <div class="account-details">
+        <p><strong>Username:</strong> johndoe</p>
+        <p><strong>Email:</strong> johndoe@example.com</p>
+        <p><strong>First Name:</strong> John</p>
+        <p><strong>Last Name:</strong> Doe</p>
+        <p><strong>Date of Birth:</strong> January 1, 1990</p>
+        <p><strong>Address:</strong> 123 Main Street, City, Country</p>
+        <!-- Add more account details here -->
+    </div>
+    <a href="edit-profile.html">Edit Profile</a>
+    <!-- Link to edit profile page -->
+</div>
+
+
+  
+  <footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <h5>Company</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Press Releases</a></li>
+                    <li><a href="#">Partnerships</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <h5>Legal</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">Terms of Service</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Cookie Policy</a></li>
+                    <li><a href="#">Copyright Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <h5>Support</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#">FAQs</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Feedback</a></li>
+                    <li><a href="#">Help Center</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <h5>Follow Us</h5>
+                <ul class="list-unstyled list-inline">
+                    <li class="list-inline-item"><a href="#" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+  
+  </body>
+  </html>
+  
